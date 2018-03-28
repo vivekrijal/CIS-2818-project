c8aeb5bbfcc51bbaa4bef9d0b9a5ebee0f3e518a
vivekrijal <vivek.rijal2@gmail.com>
2018-03-28 16:49:57 -0400

    project

---
+++ ProjectSampleMenus/app/src/main/java/com/project/menus/SecurityActivity.java
@@ -0,0 +1,39 @@
+package com.project.menus;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SecurityActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_health:
+                startActivity(new Intent(getApplicationContext(), HealthActivity.class));
+                return true;
+            case R.id.menu_security:
+                startActivity(new Intent(getApplicationContext(), SecurityActivity.class));
+                return true;
+            case R.id.menu_energy:
+                startActivity(new Intent(getApplicationContext(), EnergyActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
